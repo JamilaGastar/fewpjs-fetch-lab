626536b8ffd168dbafd1c2955dfd00c0c2e05687
jamilagastar <jamilagastar@gmail.com>
2022-05-14 13:50:28 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const books = 'https://anapioficeandfire.com/api/books'
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(books)
+  .then(resp => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
